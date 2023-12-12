807195c6ea57cc2aac444b4ccedf45521500d649
linfengfan <lin.fengfan@i-jimi.com.cn>
2023-12-12 18:12:47 +0800

    增加常用样式、引入icon font、ICON组件

---
+++ miniprogram/components/Icon/index.ts
@@ -0,0 +1,17 @@
+// components/Icon/index.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    icon: String,
+    size: {
+      type: String,
+      value: '32rpx'
+    },
+    color: {
+      type: String,
+      value: ''
+    }
+  }
+})
